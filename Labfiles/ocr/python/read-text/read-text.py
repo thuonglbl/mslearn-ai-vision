0c8a3774f2a3bb54489162429e2cfba9a415ee2f
Lam Ba Le Thuong <36751158+thuonglbl@users.noreply.github.com>
2025-10-24 17:10:29 +0700

    Integrate Azure AI Vision for text extraction
    Added Azure AI Vision client to read text from images.

--- Labfiles/ocr/python/read-text/read-text.py
+++ Labfiles/ocr/python/read-text/read-text.py
@@ -6,8 +6,9 @@ from PIL import Image, ImageDraw
 from matplotlib import pyplot as plt
 
 # import namespaces
-
-
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
 
 def main():
 
@@ -27,14 +28,35 @@ def main():
 
 
         # Authenticate Azure AI Vision client
-
+        cv_client = ImageAnalysisClient(
+            endpoint=ai_endpoint,
+            credential=AzureKeyCredential(ai_key))
         
         # Read text in image
-        
+        with open(image_file, "rb") as f:
+            image_data = f.read()
+        print (f"\nReading text in {image_file}")
+
+        result = cv_client.analyze(
+            image_data=image_data,
+            visual_features=[VisualFeatures.READ])        
 
         # Print the text
-        
+        if result.read is not None:
+            print("\nText:")
+    
+        for line in result.read.blocks[0].lines:
+            print(f" {line.text}")        
+        # Annotate the text in the image
+        annotate_lines(image_file, result.read)
 
+        # Find individual words in each line
+        print ("\nIndividual words:")
+        for line in result.read.blocks[0].lines:
+            for word in line.words:
+                print(f"  {word.text} (Confidence: {word.confidence*100:.2f}%)")
+        # Annotate the words in the image
+        annotate_words(image_file, result.read)
 
     except Exception as ex:
         print(ex)
