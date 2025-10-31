d60effc996636230c5f033de9bb9f9a0ca9376fc
Lam Ba Le Thuong <36751158+thuonglbl@users.noreply.github.com>
2025-10-31 16:54:14 +0700

    Integrate Azure AI client and image processing
    Added Azure AI client integration for chat functionality with image input processing.

--- Labfiles/gen-ai-vision/python/chat-app.py
+++ Labfiles/gen-ai-vision/python/chat-app.py
@@ -5,7 +5,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Add references
-
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from openai import AzureOpenAI
 
 def main(): 
 
@@ -21,11 +23,17 @@ def main():
 
 
         # Initialize the project client
-
+        project_client = AIProjectClient(            
+            credential=DefaultAzureCredential(
+                exclude_environment_credential=True,
+                exclude_managed_identity_credential=True
+            ),
+            endpoint=project_endpoint,
+        )
         
 
         # Get a chat client
-        
+        openai_client = project_client.get_openai_client(api_version="2024-10-21")
 
 
 
@@ -45,7 +53,23 @@ def main():
 
 
                 # Get a response to image input
-                    
+                image_url = "https://github.com/MicrosoftLearning/mslearn-ai-vision/raw/refs/heads/main/Labfiles/gen-ai-vision/orange.jpeg"
+                image_format = "jpeg"
+                request = Request(image_url, headers={"User-Agent": "Mozilla/5.0"})
+                image_data = base64.b64encode(urlopen(request).read()).decode("utf-8")
+                data_url = f"data:image/{image_format};base64,{image_data}"
+
+                response = openai_client.chat.completions.create(
+                    model=model_deployment,
+                    messages=[
+                        {"role": "system", "content": system_message},
+                        { "role": "user", "content": [  
+                            { "type": "text", "text": prompt},
+                            { "type": "image_url", "image_url": {"url": data_url}}
+                        ] } 
+                    ]
+                )
+                print(response.choices[0].message.content)                    
 
 
     except Exception as ex:
